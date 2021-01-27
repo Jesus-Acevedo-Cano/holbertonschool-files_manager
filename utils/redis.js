9691993ac7a40808c81297e380f4cd31e1fb2038
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2021-01-27 17:16:15 -0500

    First commit

--- utils/redis.js
+++ utils/redis.js
@@ -1,21 +1,25 @@
-const redis = require("redis");
+const redis = require('redis');
 
 class RedisClient {
   constructor() {
     this.client = redis.createClient();
     this.client.on('error', (error) => console.log(error));
   }
+
   isAlive() {
     return this.client.connected;
   }
+
   async get(key) {
     return this.client.get(key) || null;
   }
+
   async set(key, value, duration) {
     this.client.set(key, value, 'EX', duration);
   }
+
   async del(key) {
-    this.client.del(key);
+    if (this.get(key)) this.client.del(key);
   }
 }
 
