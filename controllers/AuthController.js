3bcbda3fb7e3b67ccd786768c24438a44362d35d
dantsub <dany.mrtinez@gmail.com>
2021-01-28 23:56:09 -0500

    task 4 auth

---
+++ controllers/AuthController.js
@@ -0,0 +1,44 @@
+import sha1 from 'sha1';
+import uuidv4 from 'uuid';
+import db from '../utils/db';
+import check from '../utils/check';
+import redis from '../utils/redis';
+
+class AuthController {
+  static async getConnect(req, res) {
+    const auth = req.header('Authorization') || null;
+    if (!auth) return check.unauthorized(res);
+
+    const buff = Buffer.from(auth.replace('Basic ', ''), 'base64');
+    const credentials = {
+      email: buff.toString('utf-8').split(':')[0],
+      password: buff.toString('utf-8').split(':')[1],
+    };
+
+    if (!credentials.email || !credentials.password) return check.unauthorized(res);
+
+    credentials.password = sha1(credentials.password);
+
+    const userExists = await db.getUser(credentials);
+    if (!userExists) return check.unauthorized(res);
+
+    const token = uuidv4();
+    const key = `auth_${token}`;
+    await redis.set(key, userExists._id.toString(), 86400);
+
+    return response.status(200).send({ token });
+  }
+
+  static async getDisconnect(req, res) {
+    const xToken = req.header('X-Token') || null;
+    if (!xToken) return check.unauthorized(res);
+
+    const authToken = await redis.get(`auth_${xToken}`);
+    if (!authToken) return check.unauthorized(res);
+
+    await redis.del(`auth_${authToken}`);
+    return res.status(204).send();
+  }
+}
+
+module.exports = AuthController;
--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,9 @@ const router = (app) => {
   paths.get('/status', ((req, res) => AppController.getStatus(req, res)));
   paths.get('/stats', ((req, res) => AppController.getStats(req, res)));
   paths.post('/users', ((req, res) => UsersController.postNew(req, res)));
+  paths.get('/connect', ((req, res) => AuthController.getConnect(req, res)));
+  paths.get('/disconnect', ((req, res) => AuthController.getDisconnect(req, res)));
+  paths.get('/users/me', ((req, res) => UsersController.getMe(req, res)));
 };
 
 export default router;
